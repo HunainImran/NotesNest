ae3284e81a1ee709869af9ba29fa5ae9586ff811
HunainImran <hunaindangra@gmail.com>
2024-07-18 19:34:15 +0500

    Navbar created

---
+++ frontend/notes-app/src/components/SearchBar.tsx
@@ -0,0 +1,33 @@
+import React, { ChangeEvent } from 'react';
+import { FaMagnifyingGlass } from 'react-icons/fa6';
+import {IoMdClose} from 'react-icons/io';
+
+// Define the types for the props
+interface SearchBarProps {
+  value: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  handleSearch: () => void;
+  onClearSearch?: () => void; // Optional prop
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ value, onChange, handleSearch, onClearSearch }) => {
+  return (
+    <div className='w-80 flex items-center px-4 bg-slate-100 rounded-md'>
+      <input 
+        type="text" 
+        placeholder='Search Notes' 
+        className='w-full text-xs bg-transparent py-[11px] outline-none' 
+        value={value} 
+        onChange={onChange} 
+      />
+
+      {value && (<IoMdClose className='text-al text-slate-500 cursor-pointer hover:text-black mr-3' onClick={onClearSearch} size={20}/>)}
+      <FaMagnifyingGlass 
+        className='text-slate-400 cursor-pointer hover:text-black' 
+        onClick={handleSearch} 
+      />
+    </div>
+  );
+}
+
+export default SearchBar;
