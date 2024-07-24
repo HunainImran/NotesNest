241fff3c9a0d7adf3e66b9e736926477f95312ea
HunainImran <hunaindangra@gmail.com>
2024-07-24 05:58:02 +0500

    Login API Integrated

--- frontend/notes-app/src/pages/Home.tsx
+++ frontend/notes-app/src/pages/Home.tsx
@@ -1,18 +1,51 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar';
 import NoteCard from '../components/NoteCard';
 import { MdAdd } from 'react-icons/md';
 import Modal from 'react-modal';
 import NotePopup from '../components/NotePopup';
+import { useNavigate } from 'react-router-dom';
+import axiosInstance from '../utils/axiosInstance';
 
 function Home() {
+  // Define the type for userInfo
+interface UserInfo {
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  // Add other properties as needed
+}
+  const [openAddEditModal, setOpenAddEditModal] = useState({ isShown: false, type: "add", data: null });
+  const [userInfo, setUserInfo] = useState<UserInfo | null>(null); 
+  const navigate = useNavigate();
 
-  const [openAddEditModal, setOpenAddEditModal]  = useState({isShown:false, type:"add", data:null})
+  const getUserInfo = async () => {
+    try {
+      const response = await axiosInstance.get("/get-user");
+      if (response.data) {
+        setUserInfo(response.data);
+        console.log("USER INFO STATE : ", userInfo);
+      }
+    } catch (error: any) {
+      if (error.response?.status === 401) {
+        localStorage.clear();
+        navigate('/login');
+      } else {
+        console.error("Error fetching user info:", error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    getUserInfo();
+  }, []); 
+
+
   return (
     <>
-      <Navbar />
-      <div className="container mx-auto px-6"> 
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8'> 
+      <Navbar userInfo={userInfo} />
+      <div className="container mx-auto px-6">
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8'>
           <NoteCard 
             title="Complete the project"
             date="2nd August 2024"
@@ -56,13 +89,13 @@ function Home() {
         </div>
       </div>
 
-      <button className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 fixed right-10 bottom-10" onClick={()=>{setOpenAddEditModal({isShown:true, type:"add", data:null})}}>
-        <MdAdd className="text-[32px] text-white"/>
+      <button className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 fixed right-10 bottom-10" onClick={() => { setOpenAddEditModal({ isShown: true, type: "add", data: null }) }}>
+        <MdAdd className="text-[32px] text-white" />
       </button>
 
       <Modal
         isOpen={openAddEditModal.isShown}
-        onRequestClose={()=>{}}
+        onRequestClose={() => {}}
         style={{
           overlay: {
             backgroundColor: "rgba(0,0,0,0.2)",
@@ -71,11 +104,10 @@ function Home() {
         contentLabel=""
         className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll"
       >
-        <NotePopup noteData={openAddEditModal.data} type={openAddEditModal.type}  onClose={() => {
-          setOpenAddEditModal({isShown:false, type: "add", data:null})
-        }}/>
+        <NotePopup noteData={openAddEditModal.data} type={openAddEditModal.type} onClose={() => {
+          setOpenAddEditModal({ isShown: false, type: "add", data: null })
+        }} />
       </Modal>
-
     </>
   );
 }
--- frontend/notes-app/src/pages/SignIn.tsx
+++ frontend/notes-app/src/pages/SignIn.tsx
@@ -11,11 +11,15 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { deepmerge } from '@mui/utils';
+import { ThemeProvider } from '@mui/material/styles';
 import human3 from '../assets/human-3.svg';
 import human2 from '../assets/human-2.svg';
 import customTheme from '../utils/theme';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
+import axiosInstance from '../utils/axiosInstance';
+import { Password } from '@mui/icons-material';
+
+
 
 function Copyright(props: any) {
   return (
@@ -32,13 +36,31 @@ function Copyright(props: any) {
 
 
 export default function SignInSide() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
       email: data.get('email'),
       password: data.get('password'),
     });
+    try{
+      const response = await axiosInstance.post("/login", {
+        email: data.get('email'),
+        password: data.get('password')
+      });
+      if (response.data && response.data.accessToken){
+        localStorage.setItem("token", response.data.accessToken);
+        navigate('/dashboard');
+      }
+    }
+    catch(error: any){
+      if (error.response && error.response.data && error.response.data.message){
+        console.log(error)
+      }
+    }
   };
 
   return (
