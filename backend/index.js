a556cc8a5a238e8f91de6052b0567c3f16789ff6
HunainImran <hunaindangra@gmail.com>
2024-07-26 15:40:37 +0500

    search note api created

--- backend/index.js
+++ backend/index.js
@@ -190,5 +190,35 @@ app.get("/get-user", authenticateToken, async (req,res) => {
     return res.status(200).json({firstName: isUser.firstName, lastName: isUser.lastName, email: isUser.email});
 });
 
+// Search Note Api
+app.get('/search-notes', authenticateToken, async (req, res) => {
+    const user = req.user;
+    const searchQuery = req.query.q;
+
+    if (!searchQuery) {
+        return res.status(400).json({ error: true, message: "Search query is required" });
+    }
+
+    try {
+        const regex = new RegExp(searchQuery, "i");
+        const matchingNotes = await note.find({
+            userId: user._id,
+            $or: [
+                { title: { $regex: regex } },
+                { content: { $regex: regex } }
+            ]
+        });
+
+        return res.json({ error: false, notes: matchingNotes, message: "Notes matching the search query retrieved successfully" });
+    } catch (err) {
+        console.error("Error searching notes:", err);
+        return res.status(500).json({
+            error: true,
+            message: "Internal server error",
+        });
+    }
+});
+
+
 app.listen(8000);
 module.exports = app;
